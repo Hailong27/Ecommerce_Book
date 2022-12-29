14f51e407cf678fe3f03789fb0760fab1fc18c94
Hailong27 <89600044+Hailong27@users.noreply.github.com>
2022-12-29 15:24:14 +0700

    backend

---
+++ backend/src/main/java/com/example/library_pr/database/Database.java
@@ -0,0 +1,29 @@
+package com.example.library_pr.database;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.library_pr.models.Book;
+import com.example.library_pr.repositories.BookRepository;
+
+@Configuration
+public class Database {
+	private static final Logger logger = LoggerFactory.getLogger(Database.class);
+	
+	@Bean
+	CommandLineRunner initDatabase(BookRepository bookRepository) {
+		return new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+				// TODO Auto-generated method stub
+//				Book book = new Book("Hi", "Long", "Chao", "28/11/2022", 10, 5000, "Hoc Tap");
+//				logger.info("insert "+bookRepository.save(book));
+				
+			}
+		};
+	}
+}
